f04aa92f4bf3aae62a4c394025816467df96a317
Moritz Schmidt <fusselwurm@gmail.com>
2017-07-17 18:35:56 +0200

    add "no default action" module

---
+++ optionals/nodefaultaction/README.md
@@ -0,0 +1,11 @@
+ace_nodefaultaction
+===========
+
+Removes default action icon
+
+
+## Maintainers
+
+The people responsible for merging changes to this component or answering potential questions.
+
+- [fusselwurm](https://github.com/fusselwurm)
---
+++ optionals/nodefaultaction/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT nodefaultaction
+#define COMPONENT_BEAUTIFIED No Default Action
+#include "\z\ace\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_NODEFAULTACTION
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_NODEFAULTACTION
+    #define DEBUG_SETTINGS DEBUG_ENABLED_NODEFAULTACTION
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
